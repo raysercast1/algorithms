b8a46dc2734c0bb94602a4e8c2a3ff674544e608
Rayser Castrillo <rayser.alejandro@gmail.com>
2024-07-11 22:10:19 -0700

    Recursively count nodes in tree

---
+++ Leetcode/TreesAndGraphs/MaxDepth.java
@@ -0,0 +1,60 @@
+ public class MaxDepth {
+     public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node() {};
+
+        Node (int val) {
+            this.val = val;
+        }
+
+        Node (int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+     }
+
+      static public void main(String[] args) {
+        Node zero = new Node(0);
+        Node one = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        Node five = new Node(5);
+        Node six = new Node(6);
+        Node seven = new Node(7);
+
+        zero.left = one;
+        zero.right = two;
+        one.left = three;
+        one.right = four;
+        four.right = six;
+        two.right = five;
+
+        int ans = maxDepth(zero);
+        System.out.println("Printing Answer: ");
+        System.out.println(ans);   
+      }
+
+     static private int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        System.out.println("Root value is:");
+        System.out.println(root.val);
+        System.out.println("Max.max is");
+        int ans = Math.max(left, right) + 1;
+        System.out.println(ans);
+
+        return ans;
+     } 
+       
+   
+ }
+
