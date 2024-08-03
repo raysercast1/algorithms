dde4e99a9d12df356332fb1f86793005e3aff65a
Rayser Castrillo <rayser.alejandro@gmail.com>
2024-08-02 22:35:20 -0700

    Sum the nodes in the deepestLevel

---
+++ Leetcode/TreesAndGraphs/BreadthFirstSearch/DeepestLeavesSum.java
@@ -0,0 +1,120 @@
+import java.util.Queue;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class DeepestLeavesSum {
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node() {};
+
+        Node (int val) {
+            this.val = val;
+        }
+
+        Node (int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+     }
+
+    public static void main(String[] args) {
+        Node zero = new Node(0);
+        Node one = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        Node five = new Node(5);
+        Node six = new Node(6);
+        Node seven = new Node(7);
+        Node eight = new Node(8);
+        Node nine = new Node(9);
+        Node ten = new Node(10);
+        Node eleven = new Node(11);
+        Node twelve = new Node(12);
+        Node thirdteen = new Node(13);
+        Node fourteen = new Node(14);
+        Node fifteen = new Node(15);
+        Node sixteen = new Node(16);
+
+        /*
+           1
+         /   \
+        2     3
+       / \      \
+      4   5      6
+     /            \
+    7              8 
+          */  
+
+       one.left = two;
+       one.right = three;
+       two.left = four;
+       two.right = five;
+       three.right = six;
+       four.left = seven;
+       six.right = eight;
+
+       int ans = findDeepestLevelBFSAndSumDeepestNodes(ten);
+       //int ans = findDeepestLevelBFSAndSumDeepestNodes(one);
+       System.out.println(String.format("Ans: %d", ans));
+    }
+
+  
+    private static int findDeepestLevelBFSAndSumDeepestNodes(Node root) {
+        if (root == null) return 0;
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 1));
+        int deepestLevel = 0;
+        int ans = 0;
+        
+
+        //Finding deepest nodes first.
+        while(!queue.isEmpty()) {
+            Pair p = queue.poll();
+            Node n = p.n;
+            int depth = p.d;
+
+            if (n.left != null) queue.offer(new Pair(n.left, depth + 1));
+            if (n.right != null) queue.offer(new Pair(n.right, depth + 1));
+
+            deepestLevel = Math.max(deepestLevel, depth);
+        }
+        
+        //Traverse to sum the nodes in the deepest level only.
+        queue.offer(new Pair(root, 1));
+        while (!queue.isEmpty()) {
+            Pair p = queue.poll();
+            Node n = p.n;
+            int depth = p.d;
+            
+            if (deepestLevel == 1) return root.val;
+
+            if (depth == deepestLevel - 1) {
+                ans += n.left != null ? n.left.val : 0;
+                ans += n.right != null ? n.right.val : 0;
+            }
+            if (n.left != null) queue.offer(new Pair(n.left, depth + 1));
+            if (n.right != null) queue.offer(new Pair(n.right, depth + 1));
+        }
+
+
+        return ans;
+    }
+
+    private static class Pair {
+        Node n;
+        int d; 
+
+        Pair(Node n, int depth) {
+            this.n = n;
+            this.d = depth;
+        }
+    }
+}
